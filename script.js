8dacdd5d09aadf0a1672366a2d65cef59a184417
Ixi <101231278+iximillas@users.noreply.github.com>
2022-04-13 12:53:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+const menu = '<select onchange="location=this.value;"><option>Menu</option><option value="index.html">Portada</option><option value="ejemplo-1.html">Ejemplo Star Wars</option><option value="ejemplo-2.html">Ejemplo Terremotos</option><option value="ejemplo-3.html">Ejemplo Digimon</option><option value="ejemplo-4.html">Ejemplo Covid</option></option><option value="ejemplo-5.html">Tsunamis</option></select>';
+document.querySelector("body").innerHTML += menu;
